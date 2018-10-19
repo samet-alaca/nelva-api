5889b209f58169477818242962b865a9741a7ca4
samet <sametalaca77@gmail.com>
2018-10-19 22:25:05 +0200

    debug

--- src/router/index.js
+++ src/router/index.js
@@ -7,4 +7,8 @@ router.get('/test', (request, response) => {
     response.sendFile(path.join(__dirname, '/../../src/index.html'));
 });
 
+router.get('/stream/source.m3u8', (request, response) => {
+    response.sendFile(path.join(__dirname, '/../../stream/source.m3u8'));
+});
+
 export default router;
