35476bf66b734e1f72d026c3a61c27f31d5eafd8
samet <sametalaca77@gmail.com>
2018-10-19 22:28:04 +0200

    debug

--- src/class/api.js
+++ src/class/api.js
@@ -2,6 +2,7 @@ import express from 'express';
 import compression from 'compression';
 import parser from 'body-parser';
 import cors from 'cors';
+import path from 'path';
 import router from '../router';
 
 export default class API {
@@ -12,6 +13,7 @@ export default class API {
         this.api.use(parser.urlencoded({ extended: true }));
         this.api.use(cors({ exposeHeaders: ['Link'] }));
         this.api.use('/', router);
+        this.api.use('/stream', express.static(path.join(__dirname, '/../../stream')));
         this.api.listen(Number(config.port));
     }
 }
